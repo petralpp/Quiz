97d42faad10bc1f6719fe2e7e5dcc637fa1ace52
Petra <petra.paloniemi94@hotmail.com>
2026-05-14 16:06:54 +0300

    User-category hidden if not logged in

--- front/quiz-app/src/components/Quiz/CategoryNavigation.tsx
+++ front/quiz-app/src/components/Quiz/CategoryNavigation.tsx
@@ -1,6 +1,8 @@
 import { FilmIcon } from "@heroicons/react/24/outline";
 import { RocketLaunchIcon } from "@heroicons/react/24/outline";
 import type { Dispatch, SetStateAction } from "react";
+import { useAppSelector } from "../../store/hooks";
+import { selectUser } from "../../store/selectors";
 
 interface Props {
   category: string;
@@ -8,26 +10,30 @@ interface Props {
 }
 
 const CategoryNavigation = ({ category, setCategory }: Props) => {
+  const user = useAppSelector(selectUser);
+
   return (
     <div className="flex ml-2">
       <div
         onClick={() => setCategory("Education")}
-        className={`px-3 rounded-t-md font-semibold ${category === "Education" ? "bg-white" : "hover:text-gray-50"}`}
+        className={`cursor-pointer px-3 rounded-t-md font-semibold ${category === "Education" ? "bg-white" : "hover:text-gray-50"}`}
       >
         Education <RocketLaunchIcon className="size-5 inline ml-2" />
       </div>
       <div
         onClick={() => setCategory("Entertainment")}
-        className={`px-3 rounded-t-md font-semibold ${category === "Entertainment" ? "bg-white" : "hover:text-gray-50"}`}
+        className={`cursor-pointer px-3 rounded-t-md font-semibold ${category === "Entertainment" ? "bg-white" : "hover:text-gray-50"}`}
       >
         Entertainment <FilmIcon className="text-center inline size-5 ml-2" />
       </div>
-      <div
-        onClick={() => setCategory("User")}
-        className={`px-3 rounded-t-md font-semibold ${category === "User" ? "bg-white" : "hover:text-gray-50"}`}
-      >
-        My quizzes
-      </div>
+      {user && (
+        <div
+          onClick={() => setCategory("User")}
+          className={`cursor-pointer px-3 rounded-t-md font-semibold ${category === "User" ? "bg-white" : "hover:text-gray-50"}`}
+        >
+          My quizzes
+        </div>
+      )}
     </div>
   );
 };
